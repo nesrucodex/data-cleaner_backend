a5ea4c750b13a976508ace3196477ee2973d1d44
nesrucodex <nesrucodex01@gmail.com>
2025-07-26 17:53:53 +0300

    Basic public api endpoints enabled.

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
 import express from "express";
-import expressRouteErrorHandlerMiddleware from "./middlewares/expressRouteErrorHandler";
 import cors from "cors";
-import { healthRoute } from "./routes";
+
+import expressRouteErrorHandlerMiddleware from "./middlewares/expressRouteErrorHandler";
 import rateLimiter from "./middlewares/rateLimiter";
 
+import { healthRoute } from "./routes";
+import { publicApiIndexController } from "./controllers/doc.controller";
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+// 🚦 Apply rate limiting to all incoming requests
 app.use(rateLimiter);
 
+// 📘 Public API index - shows available public endpoints
+app.get("/", publicApiIndexController);
+
+// ❤️ Health check endpoint (under versioned API namespace)
 app.use("/api/v1/health", healthRoute);
 
+// 🧯 Global error handling middleware (handles thrown errors or rejected promises)
 app.use(expressRouteErrorHandlerMiddleware);
 
 export default app;
